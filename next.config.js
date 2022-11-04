2b39445953e5904dc1b955ce8832dc242011b223
Alibek <52044412+Alibek15@users.noreply.github.com>
2022-11-05 00:39:51 +0600

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withRoutes = require("nextjs-routes/config")();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-module.exports = withRoutes(nextConfig);
